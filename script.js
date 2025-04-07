8ef3dfffd74109769862fe36d2e942938fe3f968
aadebh <35617074@student.murdoch.edu.au>
2025-04-07 11:15:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let cart = [];
 let cartCount = 0;
 
-function addToCart() {
+function addToCart(productName, price) {
+  cart.push({ productName, price });
   cartCount++;
   document.getElementById("cart-count").textContent = cartCount;
+}
+
+function goToCheckout() {
+  localStorage.setItem('cart', JSON.stringify(cart)); // Store cart data in localStorage
+  window.location.href = 'checkout.html'; // Redirect to checkout page
 }
